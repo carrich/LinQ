95eeab8d70dad7eae3949d41113e12cea6aa402b
unknown <you@example.com>
2018-12-04 13:52:41 +0700

    firt upload

---
+++ linq/Context/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace linq.Context
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Models.Product> Products { get; set; }
+    }
+}
